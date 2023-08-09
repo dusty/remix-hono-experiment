fa20907343276c96d82e2bd57f7fe8b6bcc351c0
Dusty Doris <dusty@postal.io>
2023-08-09 12:58:10 -0700

    notes

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import { remix } from 'remix-hono/handler'
 
 const NODE_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
-/* type your Hono variables (used with ctx.get/ctx.set) here */
+// type your Hono variables (used with ctx.get/ctx.set) here
 type Variables = {}
 
 type ContextEnv = { Variables: Variables }
@@ -21,7 +21,7 @@ app.use('/build/*', cache(60 * 60 * 24 * 365), serveStatic({ root: './public', i
 // cache other assets for 1 hour
 app.use('/static/*', cache(60 * 60), serveStatic({ root: './public', index: '' }))
 
-// log non-assets requests
+// log non-static requests
 app.use('*', logger())
 
 // pass to remix
@@ -36,10 +36,12 @@ app.use(
   })
 )
 
+// start app, broadcast devReady in dev mode
 serve(app, () => {
   if (NODE_ENV === 'development') logDevReady(build)
 })
 
+// add cache header to the response
 function cache(seconds: number): MiddlewareHandler {
   return async function setCache(c, next) {
     await next()
